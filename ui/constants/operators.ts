59e4af0e736d890e5132cec2e2ec3577d5da2bec
David Tran <tranhuyducseven@gmail.com>
2024-06-26 12:03:15 +0700

    feat: add operators and abi constatnts

---
+++ ui/constants/operators.ts
@@ -0,0 +1,9 @@
+export const Operators: Record<number, string> = {
+  0: 'Equal',
+  1: 'Not Equal',
+  2: 'Greater Than',
+  3: 'Greater Than or Equal',
+  4: 'Less Than',
+  5: 'Less Than or Equal',
+  6: 'None',
+};
